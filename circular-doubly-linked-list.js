bef40f0f8c0b221809c11ed3f9aa2447c47d28f7
sanathecoder <sanasana33377737@gmail.com>
2026-03-06 06:01:00 +0500

    Insert At End

--- circular-doubly-linked-list.js
+++ circular-doubly-linked-list.js
@@ -5,3 +5,38 @@ class Node{
         this.prev = null
     }
 }
+
+class CircularDoublyLL{
+    constructor(){
+        this.head = null
+    }
+
+    InsertAtEnd(value){
+        let temp = new Node(value)
+        if(this.head === null){
+            this.head = temp
+            temp.next = temp
+            temp.prev = temp
+            return
+        }
+        let t = this.head
+        while(t.next !== this.head){
+            t = t.next
+        }
+
+        t.next = temp
+        temp.prev = t
+        temp.next = this.head
+        this.head.prev = temp
+       
+
+    }
+    
+
+}
+
+
+let CircularDLL = new CircularDoublyLL()
+CircularDLL.InsertAtEnd(10)
+CircularDLL.InsertAtEnd(20)
+CircularDLL.InsertAtEnd(30)
