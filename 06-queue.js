26320051bea7acfc4058886f3ab1aa8c0c398f31
sanathecoder <sanasana33377737@gmail.com>
2026-03-24 09:44:00 +0500

    queue

--- 06-queue.js
+++ 06-queue.js
@@ -7,8 +7,24 @@ class Queue{
         return this.items.length === 0
     }
 
-    insert(value){
-        this.items.insert(value)
+    Insert(value){
+         this.items.push(value)
     }
-    
+
+    Delete(){
+        if(this.IsEmpty()){
+            console.log('Queue is empty')
+        }else{
+            return this.items.shift()
+        }
+    }
 }
+
+let q = new Queue()
+q.Insert(10)
+q.Insert(20)
+q.Insert(30)
+console.log(q.Delete())
+console.log(q.Delete())
+console.log(q.Delete())
+q.Delete()
