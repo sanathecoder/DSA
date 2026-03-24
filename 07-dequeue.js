49565cc8be87c0ce464ed66772fc55056fb68383
sanathecoder <sanasana33377737@gmail.com>
2026-03-24 10:25:18 +0500

    Dequeue

---
+++ 07-dequeue.js
@@ -0,0 +1,40 @@
+class dequeue{
+    constructor(){
+        this.items = []
+    }
+
+    IsEmpty(){
+        return this.items.length === 0
+    }
+
+    InsertAtEnd(value){
+        this.items.push(value)
+    }
+
+    DeleteAtFront(){
+        if(this.IsEmpty()){
+            console.log('DeQueue is Empty')
+        }else{
+            return this.items.shift()
+        }
+    }
+    InsertAtFront(value){
+        this.items.unshift(value)
+    }
+
+    deleteAtend(){
+        if(this.IsEmpty()){
+            console.log('DeQueue is Empty')
+        }else{
+       return this.items.pop()
+    }
+    }
+}
+
+let dq = new dequeue()
+dq.InsertAtFront(10)
+dq.InsertAtFront(20)
+dq.InsertAtEnd(30)
+dq.InsertAtFront(40)
+console.log(dq.deleteAtend())
+console.log(dq.deleteAtend())
