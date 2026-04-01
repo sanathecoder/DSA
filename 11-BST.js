07f73c1d1dc453a1a35b3957c2b8879a80c9b265
sanathecoder <sanasana33377737@gmail.com>
2026-04-01 06:07:00 +0500

    BST

---
+++ 11-BST.js
@@ -0,0 +1,19 @@
+class Node{
+    constructor(value){
+        this.data = value
+        this.left = null
+        this.right = null
+    }
+
+    insert(root, value){
+        if(root == null){
+            return new Node(value)
+        }else if(root.data == value){
+            return root
+        }else if(root.data > value){
+            this.insert(root.left, value)
+        }else{
+            this.insert(root.right, value)
+        }
+    }
+}
